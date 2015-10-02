e8e8a6ed1f1f30afded56bb2279916bed0b1bad9
James Webber <bunkerprivate@gmail.com>
2015-10-02 16:27:11 +0100

    screenit: poll for document to be ready before screendumping.

--- screenit/scripts.py
+++ screenit/scripts.py
@@ -1,7 +1,8 @@
 from __future__ import print_function
-import sys, argparse, selenium, contextlib, os
+import sys, argparse, selenium, contextlib, os, time
 
 from selenium.webdriver import Remote as WebdriverRemote
+from selenium.webdriver.support.ui import WebDriverWait
 
 class ScreenitCli(object):
     """Command-line runner for screenit."""
@@ -22,13 +23,30 @@ class ScreenitCli(object):
         remote = WebdriverRemote(command_executor=settings.webdriver,
                                  desired_capabilities=capabilities)
 
+        def is_ready(driver):
+            state = driver.execute_script('return document.readyState')
+            return state == 'complete'
+
         with contextlib.closing(remote) as driver:
             for task in self.get_tasks(settings):
-                output = self.find_output_name(settings.output, task, overwrite=False)
+                try:
+                    output = self.find_output_name(settings.output, task, overwrite=False)
 
-                driver.get(task['url'])
-                print("{0} {1}".format(output, task['url']))
-                driver.save_screenshot(output)
+                    print("{0} {1}".format(output, task['url']))
+                    driver.get(task['url'])
+
+                    # Note that usin sleep seems to cause the connection to
+                    # terminate somehow.
+                    wait = WebDriverWait(driver, settings.wait_timeout)
+                    wait.until(is_ready)
+
+                    driver.save_screenshot(output)
+                except:
+                    # If we get a failure here the closing context seems to fail
+                    # as well which maens we don't get to see the outcome.
+                    import traceback
+                    traceback.print_exc()
+                    raise
 
     def get_tasks(self, settings):
         tasks = []
@@ -80,6 +98,8 @@ class ScreenitCli(object):
                             help="Location to hub or webdriver.")
         parser.add_argument("-o", "--output",
                             help="Output screenshots to this directory.  (Default pwd)")
+        parser.add_argument( "--wait-timeout", default=10, type=int,
+                            help="Time to wait until document is ready.")
         parser.add_argument("-c", "--capabilities", action="append", default=[],
                             help="Add a capability.  (Default pwd)")
         return parser
