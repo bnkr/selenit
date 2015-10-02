5f30eb3988bede7b940fee28a8e76ba328ee7115
James Webber <bunkerprivate@gmail.com>
2015-10-02 23:09:57 +0100

    build: setup.py develop works
    I think all python packaging is implictly suffixed "probably".

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
 import os, glob
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name="selenit", version="1.0.0",
       description="Selenium automation utilities.",
       long_description=open('README.rst').read(), license="MIT",
       author="James Webber", author_email="bunkerprivate@gmail.com",
-      package_dir={'servequnit': 'servequnit', 'selenibench': 'selenibench', 'screenit': 'screenit'}, 
+      packages=find_packages(exclude=["tests", "tests.*"]),
       entry_points={
           'console_scripts': [
-              'servequnit = servequnit.scriprts:servequnit_main',
-              'selenibench = selenibench.scriprts:selenibench_main',
-              'screenit = screenit.scriprts:screenit_main',
+              'servequnit = servequnit.scripts:servequnit_main',
+              'selenibench = selenibench.scripts:selenibench_main',
+              'screenit = screenit.scripts:screenit_main',
            ],
       },
       url="http://github.com/bnkr/selenit",
